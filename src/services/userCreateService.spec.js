25fc29f24f367459d65f52680ab84b406ba3ddfb
Eduardoex <eduardodelimaex@gmail.com>
2023-10-10 16:15:53 -0300

    jester

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,11 +1,42 @@
 const UserCreateService = require("./UserCreateService")
-it("user should be create", () => {
+const UserRepositoryInMemoty = require("../repositories/UserRepositoryInMemory")
+const AppError = require("../utils/AppError")
+
+describe("UserCreateService", () =>{
+  it("user should be create", async () => {
   const user = {
     name: "User test",
     email: "User@test.com",
     password: "123"
   }
-  const userCreateService = new UserCreateService()
-   const usercreated =  userCreateService.execute(user)
+    const userRepositoryInMemory = new UserRepositoryInMemoty()
+  const userCreateService = new UserCreateService(userRepositoryInMemory)
+   const userCreated =  await userCreateService.execute(user)
+
+   expect(userCreated).toHaveProperty("id")
   
+  })
+
+
+  it("user not should be create with exists email", async () => {
+
+    const user1 ={
+      name: "User Test 1 ",
+      email: "user@test.com",
+      password: "123"
+    };
+
+    const user2 ={
+      name: "User Test 2 ",
+      email: "user@test.com",
+      password: "456"
+    };
+
+    const userRepository = new UserRepositoryInMemoty
+    const userCreateService = new UserCreateService(userRepository)
+
+    await userCreateService.execute(user1)
+    await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."))
+  
+  })
 })
